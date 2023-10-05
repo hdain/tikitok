d5329fd76539b748c4ac276100d07eff31cb3430
dani <hdain.dev@gmail.com>
2023-10-05 10:13:16 +0900

    feat: Add camera permission

--- lib/router.dart
+++ lib/router.dart
@@ -1,58 +1,11 @@
 import 'package:go_router/go_router.dart';
-import 'package:tikitok/features/authentication/email_screen.dart';
-import 'package:tikitok/features/authentication/sign_up_screen.dart';
-import 'package:tikitok/features/authentication/username_screen.dart';
-import 'package:tikitok/features/users/user_profile_screen.dart';
+import 'package:tikitok/features/videos/video_recording_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
-      name: SignUpScreen.routeName,
-      path: SignUpScreen.routeURL,
-      builder: (context, state) => const SignUpScreen(),
-      routes: [
-        GoRoute(
-          path: UsernameScreen.routeURL,
-          name: UsernameScreen.routeName,
-          builder: (context, state) => const UsernameScreen(),
-          routes: [
-            GoRoute(
-              path: EmailScreen.routeURL,
-              name: EmailScreen.routeName,
-              builder: (context, state) {
-                final args = state.extra as EmailScreenArgs;
-                return EmailScreen(username: args.username);
-              },
-            ),
-          ],
-        )
-      ],
+      path: '/',
+      builder: (context, state) => const VideoRecordingScreen(),
     ),
-    // GoRoute(
-    //   path: LoginScreen.routeName,
-    //   builder: (context, state) => const LoginScreen(),
-    // ),
-    // GoRoute(
-    //   path: UsernameScreen.routeName,
-    //   builder: (context, state) => const UsernameScreen(),
-    // ),
-    // GoRoute(
-    //   path: EmailScreen.routeName,
-    //   builder: (context, state) {
-    //     final args = state.extra as EmailScreenArgs;
-    //     return EmailScreen(username: args.username);
-    //   },
-    // ),
-    GoRoute(
-      path: '/users/:username',
-      builder: (context, state) {
-        final username = state.pathParameters['username'];
-        final tab = state.uri.queryParameters['show'];
-        return UserProfileScreen(
-          username: username!,
-          tab: tab!,
-        );
-      },
-    )
   ],
 );
